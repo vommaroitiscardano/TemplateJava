20f5582f9047284c7f0474460b74fd4630c6e5f1
vommaroitiscardano <63965603+vommaroitiscardano@users.noreply.github.com>
2020-10-22 10:24:51 +0200

    Update run_button.sh

--- run_button.sh
+++ run_button.sh
@@ -1,2 +1,9 @@
-javac src/*.java
-java src/Hello
+#in questo file mettiamo i comandi che deve eseguire il buttone RUN di repl.it
+#entriamo prima alla cartella src
+#Per un progetto Java è necessario compilare il progetto utilizzando il comando javac.
+#con * .javac compiliamo tutti i file Java nella cartella.
+#in seguito eseguiamo la classe Java che desideriamo utilizzando il comando java seguito dal nome della classe.
+cd src
+
+javac *.java
+java Hello
